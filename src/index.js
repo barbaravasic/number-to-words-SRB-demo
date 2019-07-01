f2bbae7016ecf197196292959272f1c7bd4a6e3d
Barbara Vasic <barbara.vasic0108@gmail.com>
2019-07-01 16:11:20 +0200

    add input validation

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import { numberToWordsSRB } from 'number-to-words-srb'
 
 const btn = document.querySelector('.button')
 const input = document.querySelector('.input')
+
 var inputValue
 var output
+
+const inputValidate = (inputValue) => {
+
+    if(isNaN(inputValue)) {
+        return 'You must insert a number'
+    }
+}
 
 function keyup(e) {
 
     if (e.keyCode == 13) {
-        inputValue = parseInt(e.target.value);
-        output = numberToWordsSRB(inputValue)
+        inputValue = parseInt(input.value);
+        output = inputValidate(inputValue) || numberToWordsSRB(inputValue)
         document.querySelector('.result').innerHTML = output
     }
 }
@@ -18,6 +26,6 @@ input.addEventListener('keyup', keyup)
 
 btn.addEventListener('click', function (e) {
     const inputValue = parseInt(input.value)
-    output = numberToWordsSRB(inputValue)
+    output = inputValidate(inputValue) || numberToWordsSRB(inputValue)
     document.querySelector('.result').innerHTML = output
 })
